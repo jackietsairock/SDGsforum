c3f65d373b056ccf193028521566f8c8c505bc4e
jackietsai <jackietsairock@gmail.com>
2026-05-13 17:29:29 +0800

    title 修改

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -12,12 +12,15 @@
 
 <template>
     <div :class="['title_area w-full text-center',infoData.tag==='intro' ? 'pb-3 sm:pb-[15px]' : 'pb-0 sm:pb-[0px]']">
-        <div :class="['title_box w-fit mx-auto','py-5']">
+        <div :class="['title_box w-fit mx-auto py-5 px-5']">
             <component :is="headingTag" :style="{ color: infoData.titleColor}" :class="['title text-5xl leading-[1.4]']" v-html="infoData.title"></component>
         </div>
     </div>
 </template>
 
 <style scoped>
-   
+   .title_box{
+    background: url('../assets/image/title-text.png') center center no-repeat;
+    background-size: contain;
+   }
 </style>
